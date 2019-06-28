7ba3015d651f89a68f9a2225d6a14cf465333994
kenneth ugalde mejia <kenneth.ugalde@ucrso.info>
2019-06-28 11:42:43 -0600

    Cambios_kennany - rutas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import './App.css';
-import Nav from './Components/Header';
-import Carrusel from './Components/Carrusel';
+import Rutas from './Rutas/rutas';
+import { BrowserRouter as Router, Route,Switch, Redirect } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header id="header">
-        <Nav></Nav>
-      </header>
-      <body id="body">
-      <Carrusel></Carrusel>
-      </body>
-      <footer id="footer">
-        fggg
-      </footer>
-    </div>
+    <Switch>
+        <div>
+            <Route exact path="/" component={Home} />
+        </div>
+    </Switch>
   );
 }
 
 export default App;
-
