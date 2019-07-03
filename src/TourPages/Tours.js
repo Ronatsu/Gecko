a718b9f0f2dda4d3dfd493a6ead34c3d88a7aad8
Ronald Alfaro <ronald.alfarohidalgo@ucrso.info>
2019-07-03 14:52:13 -0600

    fdff

--- src/TourPages/Tours.js
+++ src/TourPages/Tours.js
@@ -6,6 +6,7 @@ import { CustomInput, FormGroup, Label, Col, Row } from 'reactstrap';
 import '../GeneralCSS/TourCss.css';
 import $ from 'jquery';
 import { Accordion, Card, Button } from "react-bootstrap";
+import EasyPagination from 'easy-pagination'
 
 
 class Tour extends Component {
@@ -81,12 +82,21 @@ class Tour extends Component {
             ]
         }
         $(document).ready(function () {
-            $("#4").click(function () {
-                /*$("#5").html(<ExpandLess/>);
-              alert("Hello!");*/
-            });
+            /*new EasyPagination(document.getElementById('pagination'), {
+                totalPage: 2,// required
+                range: 5, // default 5
+                currentPage: 1, // default 1
+                callback: page => {
+                  console.log(page)
+                },
+                prevText: '<', // default '<'
+                nextText: '>', // default '>'
+             })*/
+            
         });
+        
     }
+    
 
     render() {
         let tourCards = this.state.tour.map(tour => {
@@ -99,31 +109,56 @@ class Tour extends Component {
         return (
             <div className="container-fluid all">
                 <div className="row">
-                    <div className="sidebar col-sm-3 h-100">
-                        <div id="accordion">
-
-                            <div class="card header">
-                                <div class="card-header">
-                                    <a class="card-link" data-toggle="collapse" href="#collapseOne">
-                                        Collapsible Group Item #1
-                                    </a>
+                    <div id="accordion" className=" BGColorAccordion col-sm-3">
+                        <div className="TextHeader">
+                            <strong>Filtros</strong>
+                        </div>
+                        <div className="card">
+                            <div className="card-header" data-toggle="collapse" href="#collapseOne">
+                                Checkboxes
+                            </div>
+                            <div id="collapseOne" className="collapse show">
+                                <div className="card-body">
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox" >Montaña</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox2">Laguna</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox3">Caminata</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox4">Rio</CustomInput>
                                 </div>
-                                <div id="collapseOne" class="collapse show" data-parent="#accordion">
-                                    <div class="card-body">
-                                        <CustomInput type="checkbox" id="exampleCustomCheckbox" >Montaña</CustomInput>
-                                        <CustomInput type="checkbox" id="exampleCustomCheckbox2">Laguna</CustomInput>
-                                        <CustomInput type="checkbox" id="exampleCustomCheckbox3">Caminata</CustomInput>
-                                        <CustomInput type="checkbox" id="exampleCustomCheckbox4">Rio</CustomInput>
-                                    </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-header " data-toggle="collapse" href="#collapseOne2">
+                                Checkboxes
+                            </div>
+                            <div id="collapseOne2" className="collapse show" >
+                                <div className="card-body">
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox" >Montaña</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox2">Laguna</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox3">Caminata</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox4">Rio</CustomInput>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-header " data-toggle="collapse" href="#collapseOne3">
+                                Checkboxes
+                            </div>
+                            <div id="collapseOne3" className="collapse show" >
+                                <div className="card-body">
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox" >Montaña</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox2">Laguna</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox3">Caminata</CustomInput>
+                                    <CustomInput type="checkbox" id="exampleCustomCheckbox4">Rio</CustomInput>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm-9 ml-auto content">
-                        <Row>
+
+                    <div className="col-sm-9 ml-auto content" >
+                        <Row >
                             {tourCards}
                         </Row>
-                    </div>
+                    </div >
                 </div>
             </div>
         );
